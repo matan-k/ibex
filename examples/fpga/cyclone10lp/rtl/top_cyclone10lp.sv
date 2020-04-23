f852573c404affa17303e71cff3b8eab90493732
matan k <matanka12@gmail.com>
2020-04-23 11:45:25 +0300

    Ibex core on Cyclone10LP EVB works for basic programs. Known limitation for string manipulation. [top_cyclone10lp]: adjusted absolute addresses to relative ones for the RAM. [link.ld]: based on common directory's link.ld, with adjustments to the address ranges in the design. [led/Makefile]: now generates 32-bit words isntead of the default 8-bit. [led/*.c,led/*.h] : added support for basic functions from simple_system_common and strings common functions.

--- examples/fpga/cyclone10lp/rtl/top_cyclone10lp.sv
+++ examples/fpga/cyclone10lp/rtl/top_cyclone10lp.sv
@@ -11,8 +11,7 @@ module top_cyclone10lp (
   parameter int          MEM_SIZE  = 64 * 1024; // 64 kB
   parameter logic [31:0] MEM_START = 32'h00000000;
   parameter logic [31:0] MEM_MASK  = MEM_SIZE-1;
-
-  // logic IO_CLK, IO_RST_N;
+  parameter logic [31:0] RAM_OFFSET = 'h0000C000;
 
   // Instruction connection to SRAM
   logic        instr_req;
@@ -31,119 +30,103 @@ module top_cyclone10lp (
   logic [31:0] data_wdata;
   logic [31:0] data_rdata;
 
-  // SRAM arbiter
-  logic [31:0] mem_addr;
-  logic        mem_req;
-  logic        mem_write;
-  logic  [3:0] mem_be;
-  logic [31:0] mem_wdata;
-  logic        mem_rvalid;
-  logic [31:0] mem_rdata;
-  logic        mem_read;
+  // ROM arbiter
+  logic [31:0] instr_mem_addr;
+  logic        instr_mem_req;
+  logic        instr_mem_write;
+  logic  [3:0] instr_mem_be;
+  logic [31:0] instr_mem_wdata;
+  logic        instr_mem_rvalid;
+  logic [31:0] instr_mem_rdata;
+  logic        instr_mem_read;
+
+  // RAM arbiter
+  logic [31:0] data_mem_addr;
+  logic        data_mem_req;
+  logic        data_mem_write;
+  logic  [3:0] data_mem_be;
+  logic [31:0] data_mem_wdata;
+  logic        data_mem_rvalid;
+  logic [31:0] data_mem_rdata;
+  logic        data_mem_read;
+
+  // LED events
+  logic led_event;
 
 
+  ibex_core #(
+     .DmHaltAddr(32'h00000000),
+     .DmExceptionAddr(32'h00000000)
+  ) u_core (
+     .clk_i                 (IO_CLK),
+     .rst_ni                (IO_RST_N),
 
-  // ibex_core #(
-  //    .DmHaltAddr(32'h00000000),
-  //    .DmExceptionAddr(32'h00000000)
-  // ) u_core (
-  //    .clk_i                 (IO_CLK),
-  //    .rst_ni                (IO_RST_N),
+     .test_en_i             ('b0),
 
-  //    .test_en_i             ('b0),
+     .hart_id_i             (32'b0),
+     // First instruction executed is at 0x0 + 0x80
+     .boot_addr_i           (32'h00000000),
 
-  //    .hart_id_i             (32'b0),
-  //    // First instruction executed is at 0x0 + 0x80
-  //    .boot_addr_i           (32'h00000000),
+     .instr_req_o           (instr_req),
+     .instr_gnt_i           (instr_gnt),
+     .instr_rvalid_i        (instr_rvalid),
+     .instr_addr_o          (instr_addr),
+     .instr_rdata_i         (instr_rdata),
+     .instr_err_i           ('b0),
 
-  //    .instr_req_o           (instr_req),
-  //    .instr_gnt_i           (instr_gnt),
-  //    .instr_rvalid_i        (instr_rvalid),
-  //    .instr_addr_o          (instr_addr),
-  //    .instr_rdata_i         (instr_rdata),
-  //    .instr_err_i           ('b0),
+     .data_req_o            (data_req),
+     .data_gnt_i            (data_gnt),
+     .data_rvalid_i         (data_rvalid),
+     .data_we_o             (data_we),
+     .data_be_o             (data_be),
+     .data_addr_o           (data_addr),
+     .data_wdata_o          (data_wdata),
+     .data_rdata_i          (data_rdata),
+     .data_err_i            ('b0),
 
-  //    .data_req_o            (data_req),
-  //    .data_gnt_i            (data_gnt),
-  //    .data_rvalid_i         (data_rvalid),
-  //    .data_we_o             (data_we),
-  //    .data_be_o             (data_be),
-  //    .data_addr_o           (data_addr),
-  //    .data_wdata_o          (data_wdata),
-  //    .data_rdata_i          (data_rdata),
-  //    .data_err_i            ('b0),
+     .irq_software_i        (1'b0),
+     .irq_timer_i           (1'b0),
+     .irq_external_i        (1'b0),
+     .irq_fast_i            (15'b0),
+     .irq_nm_i              (1'b0),
 
-  //    .irq_software_i        (1'b0),
-  //    .irq_timer_i           (1'b0),
-  //    .irq_external_i        (1'b0),
-  //    .irq_fast_i            (15'b0),
-  //    .irq_nm_i              (1'b0),
+     .debug_req_i           ('b0),
 
-  //    .debug_req_i           ('b0),
-
-  //    .fetch_enable_i        ('b1),
-  //    .core_sleep_o          ()
-  // );
-
-  // Connect Ibex to SRAM
-  // always_comb begin
-  //   mem_req        = 1'b0;
-  //   mem_addr       = 32'b0;
-  //   mem_write      = 1'b0;
-  //   mem_be         = 4'b0;
-  //   mem_wdata      = 32'b0;
-  //   if (instr_req) begin
-  //     mem_req        = (instr_addr & ~MEM_MASK) == MEM_START;
-  //     mem_addr       = instr_addr;
-  //   end else if (data_req) begin
-  //     mem_req        = (data_addr & ~MEM_MASK) == MEM_START;
-  //     mem_write      = data_we;
-  //     mem_be         = data_be;
-  //     mem_addr       = data_addr;
-  //     mem_wdata      = data_wdata;
-  //   end
-  // end
-
-  // temp
-  // always_comb begin
-    // mem_req        = 1'b0;
-    // mem_addr       = 32'b0;
-    // mem_write      = 1'b0;
-    // mem_be         = 4'b0;
-    // mem_wdata      = 32'b0;
-    // // if (instr_req) begin
-    //   mem_req        = (instr_addr & ~MEM_MASK) == MEM_START;
-    //   mem_addr       = instr_addr;
-    // // end else if (data_req) begin
-    // //   mem_req        = (data_addr & ~MEM_MASK) == MEM_START;
-    // //   mem_write      = data_we;
-    // //   mem_be         = data_be;
-    //   mem_addr       = data_addr;
-    //   mem_wdata      = data_wdata;
-    // end
-  // end
-
-
-  // SRAM block for instruction and data storage
-  /*ram_1p #(
-    .Depth(MEM_SIZE / 4)
-  ) u_ram (
-    .clk_i     ( IO_CLK        ),
-    .rst_ni    ( IO_RST_N      ),
-    .req_i     ( mem_req        ),
-    .we_i      ( mem_write      ),
-    .be_i      ( mem_be         ),
-    .addr_i    ( mem_addr       ),
-    .wdata_i   ( mem_wdata      ),
-    .rvalid_o  ( mem_rvalid     ),
-    .rdata_o   ( mem_rdata      )
-  );*/
-
-  jtag_bridge jtag_inst (
-    .clk_clk                       (IO_CLK),                       //             clk.clk
-    .reset_reset_n                 (IO_RST_N)
+     .fetch_enable_i        ('b1),
+     .core_sleep_o          ()
   );
-  
+
+  //Connect Ibex to SRAM
+  always_comb begin
+    instr_mem_req        = 1'b0;
+    instr_mem_addr       = 32'b0;
+    instr_mem_write      = 1'b0;
+    instr_mem_be         = 4'b0;
+    instr_mem_wdata      = 32'b0;
+    data_mem_req        = 1'b0;
+    data_mem_addr       = 32'b0;
+    data_mem_write      = 1'b0;
+    data_mem_be         = 4'b0;
+    data_mem_wdata      = 32'b0;
+    led_event           = 1'b0;
+
+    // ROM assignments
+    if (instr_req) begin
+      instr_mem_req        = (instr_addr & ~MEM_MASK) == MEM_START;
+      instr_mem_addr       = instr_addr / 4;
+    end 
+
+    // RAM assignments
+    if (data_req) begin
+      data_mem_req        = (data_addr & ~MEM_MASK) == MEM_START;
+      data_mem_write      = data_we;
+      data_mem_be         = data_be;
+      data_mem_addr       = (data_addr - RAM_OFFSET ) / 4; // translate absolute address to relative address
+      data_mem_wdata      = data_wdata;
+      led_event           = data_addr == RAM_OFFSET;
+    end
+  end
+
  //  on_chip_mem mem_inst (
 	// 	.clk_clk           (IO_CLK),           //    clk.clk
 	// 	.reset_reset_n     (IO_RST_N),     //  reset.reset_n
@@ -156,58 +139,60 @@ module top_cyclone10lp (
 	// 	.mem_if_byteenable (mem_be)  //       .byteenable
 	// );
 
-  // SRAM to Ibex
-  // assign instr_rdata    = mem_rdata;
-  // assign data_rdata     = mem_rdata;
-  // assign instr_rvalid   = mem_rvalid;
-  // always_ff @(posedge IO_CLK or negedge IO_RST_N) begin
-  //   if (!IO_RST_N) begin
-  //     instr_gnt    <= 'b0;
-  //     data_gnt     <= 'b0;
-  //     data_rvalid  <= 'b0;
-  //     mem_rvalid   <= 'b0;
-  //   end else begin
-  //     instr_gnt    <= instr_req && mem_req;
-  //     data_gnt     <= ~instr_req && data_req && mem_req;
-  //     data_rvalid  <= ~instr_req && data_req && mem_req;
-  //     mem_rvalid   <= instr_req && mem_req;
-  //   end
-  // end
+  jtag_bridge memories_and_jtag (
+    .clk_clk           (IO_CLK),           //    clk.clk
+    .reset_reset_n     (IO_RST_N),     //  reset.reset_n
+    .ram_if_address    (data_mem_addr),    // ram_if.address
+    .ram_if_chipselect (data_mem_req), //       .chipselect
+    .ram_if_clken      ('1),      //       .clken
+    .ram_if_write      (data_mem_write),      //       .write
+    .ram_if_readdata   (data_mem_rdata),   //       .readdata
+    .ram_if_writedata  (data_mem_wdata),  //       .writedata
+    .ram_if_byteenable (data_mem_be), //       .byteenable
+    .rom_if_address    (instr_mem_addr),    // rom_if.address
+    .rom_if_chipselect (instr_mem_req), //       .chipselect
+    .rom_if_clken      ('1),      //       .clken
+    .rom_if_write      (instr_mem_write),      //       .write
+    .rom_if_readdata   (instr_mem_rdata),   //       .readdata
+    .rom_if_writedata  (instr_mem_wdata),  //       .writedata
+    .rom_if_byteenable (instr_mem_be)  //       .byteenable
+  );
 
+  //SRAM to Ibex
+  assign instr_rdata    = instr_mem_rdata;
+  assign data_rdata     = data_mem_rdata;
   always_ff @(posedge IO_CLK or negedge IO_RST_N) begin
-    if(~IO_RST_N) begin
-      mem_rvalid <= 0;
+    if (!IO_RST_N) begin
+      instr_gnt       <= 'b0;
+      instr_rvalid    <= 'b0;
+      data_gnt        <= 'b0;
+      data_rvalid     <= 'b0;
     end else begin
-      mem_rvalid <= mem_read;
+      instr_gnt       <= instr_req && instr_mem_req;
+      instr_rvalid    <= instr_req && instr_mem_req;
+      data_gnt        <= data_req && data_mem_req;
+      data_rvalid     <= data_req && data_mem_req;
     end
   end
 
-  // Connect the LED output to the lower four bits of the most significant
-  // byte
-  // logic [3:0] leds;
-  // logic [3:0] mem_value;
-  // logic read;
-  // always_ff @(posedge IO_CLK or negedge IO_RST_N) begin
-  //   if (!IO_RST_N) begin
-  //     leds <= 4'b0;
-  //     read <= 1'b0;
-  //   end else begin
-  //     if(mem_rvalid == 1'b1 && !read) begin
-  //      // leds <= mem_rdata[8:5];
-  //      read <= 1'b1;
-  //     end
+  //Connect the LED output to the lower four bits of the most significant
+  //byte
+  logic [3:0] leds;
 
-  //     if (mem_req && data_req && data_we) begin
-  //       for (int i = 0; i < 4; i = i + 1) begin
-  //         if (data_be[i] == 1'b1) begin
-  //           leds <= data_wdata[i*8 +: 4];
-  //         end
-  //       end
-  //     end
-  //   end
-  // end
-  assign LED_N[1:0] = '1;
-  assign LED_N[3:2] = '0;
+  always_ff @(posedge IO_CLK or negedge IO_RST_N) begin
+    if (!IO_RST_N) begin
+      leds <= 4'b0;
+    end else begin
+      if (led_event && data_req && data_we) begin
+        for (int i = 0; i < 4; i = i + 1) begin
+          if (data_be[i] == 1'b1) begin
+            leds <= data_wdata[i*8 +: 4];
+          end
+        end
+      end
+    end
+  end
+  assign LED_N = leds;
 
   // Clock and reset
   // clkgen_xil7series
